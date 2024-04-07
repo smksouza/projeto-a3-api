a407c047bd3825ec708acd00045fda944453784a
Samuel Souza <smksouza.dev@gmail.com>
2024-04-06 23:24:19 -0300

    UserController:Create body schema

--- src/controllers/create-user.controller.ts
+++ src/controllers/create-user.controller.ts
@@ -7,14 +7,27 @@ import {
 } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { hash } from 'bcryptjs'
+import { z } from 'zod'
+
+const createUserBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  cpf: z.string().length(11),
+  phone: z.string(),
+  DistrictCouncil: z.string(),
+})
+
+type CreateUserBodySchema = z.infer<typeof createUserBodySchema>
 
 @Controller('/user')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
-    const { name, email, password, cpf, DistrictCouncil, phone } = body
+  async handle(@Body() body: CreateUserBodySchema) {
+    const { name, email, password, cpf, DistrictCouncil, phone } =
+      createUserBodySchema.parse(body)
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
@@ -22,9 +35,15 @@ export class CreateAccountController {
       },
     })
 
-    if (userWithSameEmail) {
+    const cpfAlreadyExists = await this.prisma.user.findUnique({
+      where: {
+        cpf,
+      },
+    })
+
+    if (userWithSameEmail || cpfAlreadyExists) {
       throw new ConflictException(
-        'User with same e-mail address already exists.',
+        'User with same e-mail address or CPF already exists.',
       )
     }
 
